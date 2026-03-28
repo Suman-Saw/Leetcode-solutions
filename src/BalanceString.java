f5f8e1b879046bfe2811389828ae890a972ab521
sumansaw <sumanchandra444@gmail.com>
2026-03-28 18:06:25 -0400

    BalanceString solution using stack

--- src/BalanceString.java
+++ src/BalanceString.java
@@ -1,7 +1,10 @@
+import java.util.Stack;
+
 public class BalanceString {
     public static void main(String[] args) {
-        String str = "(ab)c))";
+        String str = "(ab(()";
         System.out.println(getBalanceString(str));
+        System.out.println(balanceStringUsingStack(str));
     }
 
     private static String getBalanceString(String str) {
@@ -29,5 +32,26 @@ public class BalanceString {
             finalResult.append(c);
         }
         return finalResult.reverse().toString();
+    }
+    private static String balanceStringUsingStack(String str) {
+        StringBuilder result = new StringBuilder(str);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < result.length(); i++) {
+            char c = result.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }else {
+                    result.setCharAt(i, '\0');
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.setCharAt(stack.pop(),'\0');
+        }
+
+        return result.toString().replace("\0", "");
     }
 }
