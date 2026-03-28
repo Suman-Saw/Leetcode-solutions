8b34a98ba0aa23076018712bec04581e31d2f1bc
sumansaw <sumanchandra444@gmail.com>
2026-03-27 23:24:17 -0400

    FindSecondHighestNumber solution using priority Queue

--- src/FindSecondHighestNumber.java
+++ src/FindSecondHighestNumber.java
@@ -1,7 +1,10 @@
+import java.util.PriorityQueue;
+
 public class FindSecondHighestNumber {
     public static void main(String[] args) {
-        int[] arr = {2,6,1,11,14,7,9,13,20};
+        int[] arr = {2,6,1,11,14,7,9,13};
         System.out.println(findSecondHighestNumber(arr));
+        System.out.println(findSecondHighestNumberUsingPriorityQueue(arr));
     }
 
     private static int findSecondHighestNumber(int[] arr) {
@@ -19,5 +22,17 @@ public class FindSecondHighestNumber {
             }
         }
         return secondMax;
+    }
+    private static int findSecondHighestNumberUsingPriorityQueue(int[] arr) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int x : arr) {
+            if (!priorityQueue.contains(x)) {
+                priorityQueue.add(x);
+                if (priorityQueue.size() > 2) {
+                    priorityQueue.poll();
+                }
+            }
+        }
+        return priorityQueue.peek();
     }
 }
